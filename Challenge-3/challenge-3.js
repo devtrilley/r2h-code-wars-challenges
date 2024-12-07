b29338042fd37410018d89ca051cfe8c68f4a264
Tamsir Rilley <tamsirrilley@gmail.com>
2024-12-07 10:39:39 -0500

    Challenge 3 completed

---
+++ Challenge-3/challenge-3.js
@@ -0,0 +1,22 @@
+/*
+  Challenge 3 Prompt:
+  Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+  For example, if this array were passed as an argument:
+
+  ["Telescopes", "Glasses", "Eyes", "Monocles"]
+  Your function would return the following array:
+
+  ["Eyes", "Glasses", "Monocles", "Telescopes"]
+  All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+*/
+
+function sortByLength(array) {
+  // Return an array containing the same strings,
+  // ordered from shortest to longest
+
+  // Sort Method sorts array from shortest to greatest string by using .length property
+  return array.sort((a, b) => a.length - b.length);
+}
+
+console.log(sortByLength(["Telescopes", "Glasses", "Eyes", "Monocles"]));
