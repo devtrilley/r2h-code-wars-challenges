2638ef239bc60689aeeb97da593b791f3c5807ff
Tamsir Rilley <tamsirrilley@gmail.com>
2024-12-07 11:42:05 -0500

    Challenge 4 and 5 completed

---
+++ Challenge-4/challenge-4.js
@@ -0,0 +1,27 @@
+/*
+  Challenge 4 Prompt:
+  Task
+  Given an array/list of integers, find the Nth smallest element in the array.
+
+  Notes
+  Array/list size is at least 3.
+  Array/list's numbers could be a mixture of positives , negatives and zeros.
+  Repetition in array/list's numbers could occur, so don't remove duplications.
+
+  Input >> Output Examples
+  arr=[3,1,2]            n=2    ==> return 2 
+  arr=[15,20,7,10,4,3]   n=3    ==> return 7 
+  arr=[2,169,13,-5,0,-1] n=4    ==> return 2 
+  arr=[2,1,3,3,1,2],     n=3    ==> return 2 
+*/
+
+function nthSmallest(arr, pos) {
+  arr = arr.sort((a, b) => a - b); // Sorts array's number from least - greatest.
+
+  return arr[pos - 1]; // Returns the element at then nth-smallest index. Position - 1 will offset to give the correct element array index.
+}
+
+console.log(nthSmallest([3, 1, 2], 2));
+console.log(nthSmallest([15, 20, 7, 10, 4, 3], 3));
+console.log(nthSmallest([2, 169, 13, -5, 0, -1], 4));
+console.log(nthSmallest([2, 1, 3, 3, 1, 2], 3));
---
+++ Challenge-5/challenge-5.js
@@ -0,0 +1,55 @@
+/*
+  Challenge 5 Prompt:
+
+  You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+  Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+  []                                -->  "no one likes this"
+  ["Peter"]                         -->  "Peter likes this"
+  ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+  ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+  ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+  Note: For 4 or more names, the number in "and 2 others" simply increases.
+*/
+
+// names param is an array
+function likes(names) {
+  // If statement to determine what to do based on names.length
+  if (names.length === 0) {
+    return "no one likes this"; // if names.length = 0, return this message.
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`; // if names.length = 1, return this message with the name interpolated.
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`; // if names.length = 2, return this message with the 2 names interpolated.
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`; // if names.length = 3, return this message with the 3 names interpolated.
+  } else if (names.length >= 4) {
+    // if names.length = 4+, return this message with 2 names and the left over of likes minus those 2, interpolated.
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+console.log(likes(["Peter"]));
+
+console.log(likes(["Jacob", "Alex"]));
+
+console.log(likes(["Max", "John", "Mark"]));
+
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
+console.log(
+  likes([
+    "Alex",
+    "Jacob",
+    "Mark",
+    "Max",
+    "Leslie",
+    "Sam",
+    "Jason",
+    "Bradley",
+    "James",
+    "Thomas",
+    "Izzak",
+    "Israel",
+  ])
+);
