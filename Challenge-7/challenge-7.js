8bdf88ae374ea0126a7116dc511fdea6c99fe654
Tamsir Rilley <tamsirrilley@gmail.com>
2024-12-08 10:52:47 -0500

    Challenge 7 complete

---
+++ Challenge-7/challenge-7.js
@@ -0,0 +1,30 @@
+/*
+  Challenge 7 Prompt:
+
+  Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+  moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+function moveZeros(arr) {
+  // Var that holds Array with no zero in it
+  let noZeros = arr.filter((element) => element !== 0);
+
+  // Length of passed in arr minus the length of our noZeros array will tell us how many zeros we took out
+  let zerosRemoved = arr.length - noZeros.length;
+
+  // Param 'i' for iterations, meaning how many times you want 0 pushed.
+
+  function pushRepeat(repeats) {
+    // Loop that pushes 0 into our filtered array as many times as our repeats.
+    for (let i = 0; i < repeats; i++) {
+      noZeros.push(0);
+    }
+
+    return noZeros; // Returns the array
+  }
+
+  return pushRepeat(zerosRemoved); // Returns the returned array to where we call moveZeros.
+}
+
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
