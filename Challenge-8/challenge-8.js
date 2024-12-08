1f5cadcac236615dc314d2a241047df13199c8ee
Tamsir Rilley <tamsirrilley@gmail.com>
2024-12-08 15:34:12 -0500

    Challenge 8 completed and fixed paths in all HTML files

---
+++ Challenge-8/challenge-8.js
@@ -0,0 +1,42 @@
+/*
+  Challenge 8 Prompt:
+
+  Create a function that takes a number as an argument and returns a grade based on that number.
+
+  Score	Grade
+  Anything greater than 1 or less than 0.6	"F"
+  0.9 or greater	"A"
+  0.8 or greater	"B"
+  0.7 or greater	"C"
+  0.6 or greater	"D"
+  Examples:
+
+  grader(0)   should be "F"
+  grader(1.1) should be "F"
+  grader(0.9) should be "A"
+  grader(0.8) should be "B"
+  grader(0.7) should be "C"
+  grader(0.6) should be "D"
+*/
+
+function grader(score) {
+  // If statement that returns the string grade depending on the score passed.
+  // OR statement to trigger for both event
+  if (score < 0.6 || score > 1) {
+    return "F";
+  }
+  // >= is used to include decimals between score points
+  else if (score >= 0.9) {
+    return "A";
+  } else if (score >= 0.8) {
+    return "B";
+  } else if (score >= 0.7) {
+    return "C";
+  } else if (score >= 0.6) {
+    return "D";
+  }
+}
+
+console.log(grader(0.7));
+console.log(grader(0.9));
+console.log(grader(0.6));
